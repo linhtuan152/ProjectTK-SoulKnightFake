b1da6accff944daff4d2101901c5bf48c7945300
Nguyenkien23997 <37056347+Nguyenkien23997@users.noreply.github.com>
2018-05-24 22:36:49 +0700

    enemy

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -47,7 +47,7 @@ public class GameWindow extends JFrame {
     }
 
     private void addPlatforms() {
-        Map map = Map.load("resources/Map/Map TLU pro.json");
+        Map map = Map.load("resources/Map/MAP TLU.json");
         map.generate();
     }
 //    private void addplayer(){
